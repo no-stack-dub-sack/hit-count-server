4cb8cdc11ae205c2bdaf43481531fc2a9f067cab
Peter Weinberg <pweinberg633@gmail.com>
2017-11-18 18:12:09 -0500

    testing ip address comparison

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ app.post('/register-count', (req, res) => {
     new: true,
     setDefaultsOnInsert: true
   };
-  if (req.connection.remoteAddress !== '::ffff:10.43.224.30') {
-    console.log('ip address yo', req.connection.remoteAddress)
+  if (true) {
+    console.log(req.ip)
     Counter.findOneAndUpdate({}, {}, options, (err, doc) => {
       if (doc) {
         doc.total++;
