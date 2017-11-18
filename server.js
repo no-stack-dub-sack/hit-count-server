cda237ec822fd047cae388b9ec7504855d7c8ab4
Peter Weinberg <pweinberg633@gmail.com>
2017-11-18 18:21:39 -0500

    remove ip comparison logic

--- server.js
+++ server.js
@@ -33,29 +33,23 @@ app.post('/register-count', (req, res) => {
     new: true,
     setDefaultsOnInsert: true
   };
-  if (true) {
-    console.log(req.ip)
-    Counter.findOneAndUpdate({}, {}, options, (err, doc) => {
-      if (doc) {
-        doc.total++;
-        doc.host = req.headers.referer;
-        doc.save(err => {
-          if (err) {
-            console.error('There was an error updating the hit-count-server...');
-            console.error(err);
-            res.status(500);
-          } else {
-            console.log('Count updated!');
-            res.end();
-          }
-        });
-      }
-      res.end();
-    });
-  } else {
-    console.log('not counting owner\'s visit!');
+  Counter.findOneAndUpdate({}, {}, options, (err, doc) => {
+    if (doc) {
+      doc.total++;
+      doc.host = req.headers.referer;
+      doc.save(err => {
+        if (err) {
+          console.error('There was an error updating the hit-count-server...');
+          console.error(err);
+          res.status(500);
+        } else {
+          console.log('Count updated!');
+          res.end();
+        }
+      });
+    }
     res.end();
-  }
+  });
 });
 
 app.get('/get-count', (req, res) => {
